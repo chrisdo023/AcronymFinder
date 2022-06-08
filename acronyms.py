7dac561386a0d597e3cee792c8bc8349944f6648
Chris Do <do.chris023@gmail.com>
2022-06-08 13:26:07 -0700

    Included foundational Flask files

---
+++ acronyms.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+#from abbreviations import schwartz_hearst
+from schwartz_hearst import extract_abbreviation_definition_pairs
+import json
+import sys, getopt
+from venv import create
+import docx2txt
+import re
+from flask import jsonify
+import xlsxwriter
+import PyPDF2 as pdf2
+import csv
+from docx import Document
+from collections import OrderedDict
+
+#stores dictionary of acronyms and abbreviations
+dict_from_csv = {}
+
+#opens given outputfile and finds full abbreviations of acronyms
+def findAbbrev(inputfile):
+    pairs = extract_abbreviation_definition_pairs(doc_text=docx2txt.process(inputfile))
+    # pairs = ''
+    global dict_from_csv
+    dict_from_csv = OrderedDict(sorted(pairs.items(), key=lambda t: t[0]))
+    
+#opens document and returns acronyms found
+def findAcronyms(inputfile):
+    rx = r"\b[A-Z](?=([&.]?))(?:\1[A-Z])+\b"
+    list = [x.group() for x in re.finditer(rx, inputfile)]
+
+    # remove repeated acronyms from list
+    aclist = []
+    for i in list:
+        if i not in aclist:
+           aclist.append(str(i))
+        
+#iterates through given document and creates an excel workbook
+#with given outputfile name
+def createXLSX(outputfile):
+    workbook = xlsxwriter.Workbook("static/client/xlsx/" + outputfile + '.xlsx')
+    worksheet = workbook.add_worksheet()
+
+    row = 0
+    for k,v in dict_from_csv.items():
+        worksheet.write(row, 0, k)
+        worksheet.write(row, 1, v)
+        
+        row += 1
+    workbook.close()
+
+#iterates through given document and creates a word docx
+#with given outputfile name
+def createDoc(outputfile):
+    documentObj = Document()
+
+    table = documentObj.add_table(rows=1, cols=2)
+
+    check = []
+    
+    for k,v in dict_from_csv.items():
+        row_cells = table.add_row().cells
+        row_cells[0].text = k
+        row_cells[1].text = v
+
+    documentObj.add_page_break()
+
+    documentObj.save("static/client/docx/" + outputfile + '.docx')
+
+    return check
+
+
+
+
---
+++ app.py
@@ -0,0 +1,75 @@
+import os
+
+from flask import Flask
+from flask import render_template, request, jsonify, send_file, send_from_directory, abort
+from flask_dropzone import Dropzone
+from werkzeug.utils import secure_filename
+from acronyms import findAbbrev, findAcronyms, createXLSX, createDoc
+from werkzeug.exceptions import Forbidden, HTTPException, NotFound, RequestTimeout, Unauthorized
+
+app = Flask(__name__)
+app.config["CLIENT_CSV"] = "static/client/csv/"
+app.config["CLIENT_DOCX"] = "static/client/docx/"
+app.config["CLIENT_XLSX"] = "static/client/xlsx/"
+
+dropzone = Dropzone(app)
+
+#INPUT_FILE = "acronym_list.csv"
+
+OUTPUT_FILE = "output"
+
+@app.route('/')
+def main():
+    print('Request for index page received.')
+    return render_template('index.html')
+
+@app.errorhandler(NotFound)
+def page_not_found_handler(HTTPException):
+    return render_template('404.html'), 404
+
+@app.route("/upload", methods=['GET', 'POST'])
+def upload():
+    msg = ''
+    if request.method == 'POST':
+        f = request.files.get('file')
+        filePath = "static/client/uploads/"
+        f.save(filePath + f.filename)
+        filename = secure_filename(f.filename)
+
+        print('Request to upload file with name=%s ' % filename)
+
+        findAbbrev(filePath + f.filename)
+        findAcronyms(filePath + f.filename)
+
+        createXLSX(OUTPUT_FILE)
+        createDoc(OUTPUT_FILE)
+
+        try:
+            os.remove(filePath + f.filename)
+        except OSError as e:
+            print("Error: %s : %s" % (filePath, e.strerror))
+        
+    return jsonify(msg)
+
+@app.route("/get-docx", methods=['GET', 'POST'])
+def get_docx():
+    filename = "output.docx"
+    try:
+        return send_from_directory(app.config["CLIENT_DOCX"], path=filename, as_attachment=True)
+    except FileNotFoundError:
+        abort(404)
+
+@app.route("/get-xlsx", methods=['GET', 'POST'])
+def get_xlsx():
+    filename = "output.xlsx"
+    try:
+        return send_from_directory(app.config["CLIENT_XLSX"], path=filename, as_attachment=True)
+    except FileNotFoundError:
+        print("cannot find file")
+        abort(404)
+
+if __name__ == "__main__":
+  app.run()
+  
+  
+  
