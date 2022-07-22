bc4f4d90e708641e18090769dcab136d587eb840
Chris Do <do.chris023@gmail.com>
2022-07-22 16:38:10 -0700

    Updated application cosmetics

--- static/js/index.js
+++ static/js/index.js
@@ -16,8 +16,8 @@ function getXLSX() {
 }
 
 function trash() {
-    document.getElementById("file-one").className = "row fade-in-image nonvisible";
-    document.getElementById("line-one").className = "line nonvisible";
+    $("#file-one").fadeOut();
+    $("#line-one").fadeOut();
 }
 
 function getAcronyms() {
@@ -113,13 +113,16 @@ function toggleDisplay() {
         document.getElementById("dropzone").classList.remove("dark");
         document.getElementById("onboarding-container").classList.remove("o-dark");
         document.getElementById("action-one").className = "column column-action";
+        document.getElementById("help").classList.remove("dark");
+        document.getElementById("contactus").classList.remove("dark");
+        document.documentElement.classList.remove('dark');
+        var elements = document.getElementsByClassName('dz-remove');
+        elements[0].classList.remove('dark');
 
         fetch(`/set_cookie/${JSON.stringify("DarkMode")}/${JSON.stringify("False")}`)
         .then(function (response) {
             return response.text();
-        }).then(function (data) {
-            console.log(data)
-        });
+        }).then(function (data) {});
     } else {
         document.body.className = "dark";
         document.getElementById("display-mode").innerText = "Dark Mode";
@@ -127,18 +130,20 @@ function toggleDisplay() {
         document.getElementById("dropzone").classList.add("dark");
         document.getElementById("onboarding-container").classList.add("o-dark");
         document.getElementById("action-one").className = "column column-action dark";
+        document.getElementById("help").classList.add("dark");
+        document.getElementById("contactus").classList.add("dark");
+        var elements = document.getElementsByClassName('dz-remove');
+        elements[0].classList.add('dark');
 
         fetch(`/set_cookie/${JSON.stringify("DarkMode")}/${JSON.stringify("True")}`)
         .then(function (response) {
             return response.text();
-        }).then(function (data) {
-            console.log(data)
-        });
+        }).then(function (data) {});
     }
 }
 
 // View slide show
-let slideIndex = 1;
+var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -170,27 +175,41 @@ let onboardSlideIndex = 1;
 showOnboardSlides(onboardSlideIndex);
 
 function plusOnboardSlides(n) {
-  showOnboardSlides(slideIndex += n);
+  showOnboardSlides(onboardSlideIndex += n);
 }
 
 function currentOnboardSlide(n) {
-  showOnboardSlides(slideIndex = n);
+  showOnboardSlides(onboardSlideIndex = n);
 }
 
 function showOnboardSlides(n) {
     let i;
     let slides = document.getElementsByClassName("myOnboardSlides");
     let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}    
-    if (n < 1) {slideIndex = slides.length}
+    if (n > slides.length) {onboardSlideIndex = 1}    
+    if (n < 1) {onboardSlideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
     }
     for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
+    slides[onboardSlideIndex-1].style.display = "block";  
+    dots[onboardSlideIndex-1].className += " active";
+}
+
+// Displays Onboarding-Containter on Help btn click
+function displayHelp() {
+    $("#onboarding-container").fadeIn();
+    let slides = document.getElementsByClassName("myOnboardSlides");
+    let dots = document.getElementsByClassName("dot");
+    slides[3].style.display = "none"
+    slides[0].style.display = "block";
+    dots[3].className = dots[3].className.replace(" active", "");  
+    if(dots[0].className != "dot active"){
+        dots[0].className += " active";
+    }
+    onboardSlideIndex = 1;
 }
 
 // Event listeners
@@ -208,33 +227,12 @@ window.addEventListener('click', function(e){
     }
 });
 
-// Check for cookies when page loads
-function checkCookies(){
-    // Parse through cookie data for DarkMode and Onboard
-    fetch(`/get_cookie/${JSON.stringify('DarkMode')}`)
+// jquery
+$("#done").click(function() {
+    $("#onboarding-container").fadeOut();
+
+    fetch(`/set_cookie/${JSON.stringify("Onboarding")}/${JSON.stringify('True')}`)
     .then(function (response) {
         return response.text();
-    }).then(function (data) {
-        if(data == 'True'){
-            document.body.className = "dark";
-            document.getElementById("display-mode").innerText = "Dark Mode";
-            document.getElementById("dropzone").classList.add("dark");
-            document.getElementById("onboarding-container").classList.add("o-dark");
-            document.getElementById("action-one").className = "column column-action dark";            
-        }
-    });
-
-    // fetch(`/get_cookie/${JSON.stringify('Onboard')}`)
-    // .then(function (response) {
-    //     return response.text();
-    // }).then(function (data) {
-    //     if(data == 'True'){
-    //         alert("No Onboard")          
-    //     } else{
-    //         alert("Onboard")
-    //     }
-    // });
-}
-
-// Run at start of page
-checkCookies();
+    }).then(function (data) {});
+});
---
+++ static/js/start.js
@@ -0,0 +1,32 @@
+// Run at start of page
+checkCookies();
+
+// Check for cookies when page loads
+function checkCookies(){
+    // Parse through cookie data for DarkMode and Onboard
+    fetch(`/get_cookie/${JSON.stringify('DarkMode')}`)
+    .then(function (response) {
+        return response.text();
+    }).then(function (data) {
+        if(data == 'True'){
+            document.body.className = "dark";
+            document.getElementById("display-mode").innerText = "Dark Mode";
+            document.getElementById("dropzone").classList.add("dark");
+            document.getElementById("onboarding-container").classList.add("o-dark");
+            document.getElementById("action-one").className = "column column-action dark";   
+            document.getElementById("help").classList.add("dark");
+            document.getElementById("contactus").classList.add("dark");         
+        }
+    });
+
+    fetch(`/get_cookie/${JSON.stringify('Onboarding')}`)
+    .then(function (response) {
+        return response.text();
+    }).then(function (data) {
+        if(data == 'True'){
+
+        } else {
+            document.getElementById("onboarding-container").classList.remove("nonvisible");          
+        }
+    });
+}
